a5aedbd83d77e37f1c4d9300fb88f94f7edc7a95
paulboul1013 <paulboul1013@gmail.com>
2025-11-27 22:04:03 +0800

    disable ctrl-v

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ void enableRawMode() {
 
     struct termios raw=orig_termios;
     raw.c_iflag &= ~(IXON);
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
     tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
 }
