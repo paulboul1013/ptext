93c64bd2adf48b5ca670f2ed206ff70799fdbd0d
paulboul1013 <paulboul1013@gmail.com>
2025-11-28 10:00:07 +0800

    A timeout for read()

--- main.c
+++ main.c
@@ -22,22 +22,28 @@ void enableRawMode() {
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_cc[VMIN]=0;
+    raw.c_cc[VTIME]=1;
 
     tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
 }
 
 
 int main(){
-    enableRawMode();
-    char c;
-    //read one character from stdin
-    //exit is press ctrl+d or ctrl+c
-    while(read(STDIN_FILENO,&c,1)==1 && c!='q'){
+    enableRawMode();    
+
+    while(1){
+        char c='\0';
+        read(STDIN_FILENO,&c,1);
         if (iscntrl(c)) {
             printf("%d\r\n",c);
         }else{
             printf("%d ('%c')\r\n",c,c);
         }
+
+        if (c=='q') break;
+        
+
     }
 
 
