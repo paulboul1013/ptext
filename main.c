b6197b3ce4cefecbaa1c8c695b205040013ac5b7
paulboul1013 <paulboul1013@gmail.com>
2025-11-29 09:16:08 +0800

    Refactor keyboard input

--- main.c
+++ main.c
@@ -5,10 +5,15 @@
 #include <termios.h>
 #include <ctype.h>
 
-#define CTRL_KEY(k) ((k) & (0x1f))
 
+//define
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+//data
 struct termios orig_termios;
 
+
+//terminal
 void die(const char *s){
     perror(s);
     exit(1);
@@ -37,25 +42,37 @@ void enableRawMode() {
 }
 
 
+char editorReadKey(){
+    int nread;
+    char c;
+    while ((nread=read(STDIN_FILENO,&c,1))!=1){
+        if (nread==-1 && errno!=EAGAIN) die("read");
+    }
+    return c;
+}
+
+//input
+void editorProcessKeypress(){
+    char c=editorReadKey();
+    
+    switch (c) {
+        case 'q':
+            exit(0);
+            break;
+        // case CTRL_KEY('q'):
+        //     exit(0);
+        //     break;
+    }
+}
+
 int main(){
     enableRawMode();    
 
     while(1){
-        char c='\0';
+   
+        editorProcessKeypress();
 
-        if (read(STDIN_FILENO,&c,1)==-1 && errno!=EAGAIN) die("read");
-        if (iscntrl(c)) {
-            printf("%d\r\n",c);
-        }else{
-            printf("%d ('%c')\r\n",c,c);
-        }
-
-        if (c=='q'){
-            break;
-        }
-        // if (c==CTRL_KEY('q')) break;
-
-
+  
     }
 
 
