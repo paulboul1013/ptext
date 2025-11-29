0201168480f91c0420afaa91226a9098971b19a8
paulboul1013 <paulboul1013@gmail.com>
2025-11-29 09:48:44 +0800

    setting orig_termios Global state

--- main.c
+++ main.c
@@ -12,6 +12,11 @@
 //data
 struct termios orig_termios;
 
+struct editorConfig {
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 //terminal
 void die(const char *s){
@@ -24,16 +29,15 @@ void die(const char *s){
 
 
 void disableRawMode(){
-    if (tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios)==-1) die("tcsetattr");
+    if (tcsetattr(STDIN_FILENO,TCSAFLUSH,&E.orig_termios)==-1) die("tcsetattr");
 }
 
 void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO,&orig_termios)==-1) die("tcgetattr");
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO,&E.orig_termios)==-1) die("tcgetattr");
 
     atexit(disableRawMode);
 
-    struct termios raw=orig_termios;
+    struct termios raw=E.orig_termios;
     raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
