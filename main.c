1ff3789152df19120cf23da9dd3b14d0d5589486
paulboul1013 <paulboul1013@gmail.com>
2025-12-10 15:52:33 +0800

    limit the cursor out of the screen

--- main.c
+++ main.c
@@ -175,17 +175,26 @@ void editorDrawRows(struct abuf *ab) {
 void editorMoveCursor(int key){
     switch (key) {
         case ARROW_LEFT:
+        if (E.cx!=0) {
             E.cx--;
             break;
+        }
+        break;
         case ARROW_DOWN:
+        if (E.cy!=E.screenrows-1) {
             E.cy++;
-            break;
+        }
+        break;
         case ARROW_UP:
+        if (E.cy!=0) {
             E.cy--;
-            break;
+        }
+        break;
         case ARROW_RIGHT:
+        if (E.cx!=E.screencols-1) {
             E.cx++;
-            break;
+        }
+        break;
     }
 }
 
