6f5f6b3892359f02bd6979f4c91bbbcb8ca661a6
paulboul1013 <paulboul1013@gmail.com>
2025-12-16 10:42:36 +0800

    define tab-stop

--- main.c
+++ main.c
@@ -16,7 +16,7 @@
 //define
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_VERSION "0.0.1"
-
+#define KILO_TAB_STOP 8
 
 enum editorKey {
     ARROW_LEFT = 1000,
@@ -147,14 +147,14 @@ void editorUpdateRow(erow *row){
     }
 
     free(row->render);
-    row->render=malloc(row->size+tabs*7+1);
+    row->render=malloc(row->size+tabs*(KILO_TAB_STOP-1)+1);
 
 
     int idx=0;
     for (j=0;j<row->size;j++) {
         if (row->chars[j]=='\t') {
             row->render[idx++]=' ';
-            while (idx%8!=0) row->render[idx++]=' ';
+            while (idx%KILO_TAB_STOP!=0) row->render[idx++]=' ';
         }else {
             row->render[idx++]=row->chars[j];
         }
