545e3c7422d09801d158b4466fd5e005f969ce27
paulboul1013 <paulboul1013@gmail.com>
2025-12-16 09:58:02 +0800

    enable horizontal scroll

--- main.c
+++ main.c
@@ -259,9 +259,7 @@ void editorMoveCursor(int key){
         }
         break;
         case ARROW_RIGHT:
-        if (E.cx!=E.screencols-1) {
             E.cx++;
-        }
         break;
     }
 }
