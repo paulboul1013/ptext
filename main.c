e7892bdddbd50d149b10dc8139556180ff5cc0cf
paulboul1013 <paulboul1013@gmail.com>
2025-12-16 10:08:20 +0800

    Horizontal scroll-limits

--- main.c
+++ main.c
@@ -241,6 +241,10 @@ void editorDrawRows(struct abuf *ab) {
 //input
 
 void editorMoveCursor(int key){
+
+    erow *row=(E.cy > E.numrows)?NULL:&E.row[E.cy];
+
+
     switch (key) {
         case ARROW_LEFT:
         if (E.cx!=0) {
@@ -259,7 +263,9 @@ void editorMoveCursor(int key){
         }
         break;
         case ARROW_RIGHT:
+        if (row && E.cx < row->size) {
             E.cx++;
+        }
         break;
     }
 }
