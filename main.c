4fc5394e46bb5ffc58c462551e709869adf7ff03
paulboul1013 <paulboul1013@gmail.com>
2025-12-22 10:09:11 +0800

    change cx to rx

--- main.c
+++ main.c
@@ -393,6 +393,19 @@ int getWindowSize(int *rows, int *cols){
 }
 
 //row operations
+int editorRowCxToRx(erow *row,int cx){
+    int rx=0;
+    int j;
+    for(j=0;j<cx;j++){
+        if (row->chars[j]=='\t'){
+            rx+=(KILO_TAB_STOP-1)-(rx%KILO_TAB_STOP);
+        }
+        rx++;
+    }
+    return rx;
+}
+
+
 void editorAppendRow(char *s,size_t len) {
     E.row = realloc(E.row,sizeof(erow)*(E.numrows+1));
 
