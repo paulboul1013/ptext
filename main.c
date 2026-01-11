1a9f164a6eb03601f1d7a6063b07abf1c9066812
paulboul1013 <paulboul1013@gmail.com>
2026-01-11 16:37:31 +0800

    Add save to disk operation

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
@@ -62,6 +63,11 @@ struct editorConfig {
 };
 
 struct editorConfig E;
+
+//prototypes
+void editorSetStatusMessage(const char *fmt, ...);
+
+
 
 /*** append buffer ***/
 
@@ -298,12 +304,11 @@ void editorOpen(char *filename){
     char *line=NULL;
     size_t linecap=0;
     ssize_t linelen;
-    linelen=getline(&line,&linecap,fp);
     while ((linelen=getline(&line,&linecap,fp))!=-1) {
 
         while (linelen > 0 && (line[linelen-1] == '\n' ||
-        line[linelen-1] == '\r')) {
-            linelen--;
+            line[linelen-1] == '\r')) {
+                linelen--;
         }
     
 
@@ -312,6 +317,53 @@ void editorOpen(char *filename){
     }
     free(line);
     fclose(fp);
+}
+
+char *editorRowsToString(int *buflen) {
+    int totlen=0;
+    int j;
+    for(j=0;j<E.numrows;j++){
+        totlen+=E.row[j].size+1;
+    }
+    *buflen=totlen;
+    
+    char *buf=malloc(totlen);
+    char *p=buf;
+    for(j=0;j<E.numrows;j++){
+        memcpy(p,E.row[j].chars,E.row[j].size);
+        p+=E.row[j].size;
+        *p='\n';
+        p++;
+    }
+
+    return buf;
+}
+
+void editorSave() {
+    if (E.filename==NULL) {
+        return;
+    }
+
+    int len;
+    char *buf=editorRowsToString(&len);
+
+    int fd=open(E.filename,O_RDWR | O_CREAT,0644);
+    if (fd!=-1) {
+        
+        if (ftruncate(fd,len)!=-1){
+            if (write(fd,buf,len)==len){
+                close(fd);
+                free(buf);
+                editorSetStatusMessage("%d bytes writen to disk",len);
+                return;
+            }
+        }
+        close(fd);
+    }
+  
+    free(buf);
+    editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+
 }
 
 /*** output ***/
@@ -502,6 +554,10 @@ void editorProcessKeypress(){
             exit(0);
             break;
 
+        case CTRL_KEY('s'):
+            editorSave();
+            break;
+
         case HOME_KEY:
             E.cx=0;
             break;
@@ -586,7 +642,7 @@ int main(int argc,char *argv[]){
         editorOpen(argv[1]);
     }
 
-    editorSetStatusMessage("HELP:press q=quit");
+    editorSetStatusMessage("HELP:press q=quit | Ctrl+s=save");
 
 
     while(1){
