08143f3788b4eb70fffb90e5872fce28d50f5679
paulboul1013 <paulboul1013@gmail.com>
2026-02-24 09:59:11 +0800

    key delete mapping

--- main.c
+++ main.c
@@ -601,6 +601,10 @@ void editorProcessKeypress(){
         case BACKSPACE:
         case CTRL_KEY('h'):
         case DEL_KEY:
+            if (c==DEL_KEY) {
+                editorMoveCursor(ARROW_RIGHT);
+            }
+            editorDelChar();
             break;
 
         case PAGE_UP:
