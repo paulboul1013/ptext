5c823ca4069972bf582d9b75df2cf7d3deef868c
paulboul1013 <paulboul1013@gmail.com>
2026-03-03 17:01:21 +0800

    detect separator characters

--- main.c
+++ main.c
@@ -223,6 +223,11 @@ int getWindowSize(int *rows, int *cols){
 }
 
 //syntax highlighting
+int is_separator(int c){
+    return isspace(c) || c=='\0' || strchr(",.()+-/*=~%<>[];", c)!=NULL;
+}
+
+
 void editorUpdateSyntax(erow *row){
     row->hl=realloc(row->hl,row->rsize);
     memset(row->hl,HL_NORMAL,row->rsize);
@@ -230,7 +235,7 @@ void editorUpdateSyntax(erow *row){
     int i=0;
     while (i<row->rsize){
         char c=row->render[i];
-        
+
         if (isdigit(c)){
             row->hl[i]=HL_NUMBER;
         }
