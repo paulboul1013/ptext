215d7b9ac8961658f7da996d21420151f09b370a
paulboul1013 <paulboul1013@gmail.com>
2026-03-03 10:53:42 +0800

    syntax-refactoring

--- main.c
+++ main.c
@@ -46,6 +46,7 @@ typedef struct erow {
     int rsize;
     char *chars;
     char *render;
+    unsigned char *hl;
 }erow;
 
 struct editorConfig {
@@ -280,6 +281,7 @@ void editorInsertRow(int at,char *s,size_t len) {
 
     E.row[at].rsize=0;
     E.row[at].render=NULL;
+    E.row[at].hl=NULL;
     editorUpdateRow(&E.row[at]);
 
     E.numrows++;
@@ -290,6 +292,7 @@ void editorInsertRow(int at,char *s,size_t len) {
 void editorFreeRow(erow *row){
     free(row->render);
     free(row->chars);
+    free(row->hl);
 }
 
 void editorDelRow(int at){
