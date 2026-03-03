b3ae68856b90bcac4a52c30c27819b870f7d03c5
paulboul1013 <paulboul1013@gmail.com>
2026-03-03 16:40:09 +0800

    Add HL_MATCH color

--- main.c
+++ main.c
@@ -39,7 +39,8 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL=0,
-    HL_NUMBER
+    HL_NUMBER,
+    HL_MATCH
 };
 
 /*** data ***/
@@ -237,6 +238,7 @@ void editorUpdateSyntax(erow *row){
 int editorSyntaxToColor(int hl){
     switch(hl){
         case HL_NUMBER: return 31;
+        case HL_MATCH: return 34;
         default: return 37;
     }
 }
