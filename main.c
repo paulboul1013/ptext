8c4f1a94000adbe6285b962c19947d323e2659e7
paulboul1013 <paulboul1013@gmail.com>
2026-03-08 15:25:05 +0800

    Add strings flags

--- main.c
+++ main.c
@@ -45,6 +45,7 @@ enum editorHighlight {
 };
 
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
 
 /*** data ***/
 struct editorSyntax {
@@ -90,7 +91,7 @@ struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_extensions,
-        HL_HIGHLIGHT_NUMBERS      
+        HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS  
     },
 };
 
