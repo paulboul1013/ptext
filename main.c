9d0dd04f37ca3a6506113d83010b753ec8d3ce13
paulboul1013 <paulboul1013@gmail.com>
2026-03-08 14:49:52 +0800

    Add show filetype on the status bar

--- main.c
+++ main.c
@@ -687,7 +687,7 @@ void editorDrawStatusBar(struct abuf *ab){
     int len=snprintf(status,sizeof(status),"%.20s - %d lines %s",E.filename ? E.filename: "[No Name]",E.numrows
 ,E.dirty? ("modified"):"");
     
-    int rlen=snprintf(rstatus,sizeof(rstatus),"%d/%d",E.cy+1,E.numrows);
+    int rlen=snprintf(rstatus,sizeof(rstatus),"%s | %d/%d",E.syntax? E.syntax->filetype:"no filetype",E.cy+1,E.numrows);
     
     if (len>E.screencols){
         len=E.screencols;
