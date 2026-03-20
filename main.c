33f55d6379f760f87c8f996df246dcc644ced622
paulboul1013 <paulboul1013@gmail.com>
2026-03-20 15:54:36 +0800

    Add c keywords

--- main.c
+++ main.c
@@ -54,6 +54,7 @@ enum editorHighlight {
 struct editorSyntax {
     char *filetype;
     char **filematch;
+    char **keywords;
     char *singleline_comment_start;
     int flags;
 };
@@ -89,12 +90,19 @@ struct editorConfig E;
 
 //filetypes
 char *C_HL_extensions[]={".c",".h",".cpp",NULL};
+char *C_HL_keywords[] = {
+    "switch", "if", "while", "for", "break", "continue", "return", "else",
+    "struct", "union", "typedef", "static", "enum", "class", "case",
+    "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+    "void|", NULL
+};
 
 //HLDB:highlight database
 struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_extensions,
+        C_HL_keywords,
         "//",
         HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS  
     },
